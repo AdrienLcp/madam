ea1848ce1b639e0dc7c2490a75080b98301b978a
Adrien <adrienlcp@gmail.com>
2022-03-08 09:38:10 +0100

    404 done

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,13 +1,39 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import './styles.scss';
+
+import home from '../../media/nav/home.svg';
 
 const NotFound = () => {
 
   return (
-    <div className='not-found'>
-      
-    </div>
+    <section className='not-found'>
+
+      <h6 className='not-found--title'>
+        404
+      </h6>
+
+      <span className='not-found--span'>
+        Cette page n'existe pas
+      </span>
+
+      <NavLink
+        to='/'
+        className='not-found--button'
+      >
+        <div className='not-found--button--icon'>
+          <img
+            className='not-found--button--icon--img'
+            alt="Retourner à l'accueil"
+            src={home}
+          />
+        </div>
+        <p className='not-found--button--label'>
+          Revenir à l'accueil
+        </p>
+      </NavLink>
+    </section>
   );
 };
 
